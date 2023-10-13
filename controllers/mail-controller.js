6becc470327d0031067e63117e9d8e07a84741bb
Lokusok <lokusokdrok@gmail.com>
2023-10-13 18:01:06 +0300

    add recaptcha logic

--- controllers/mail-controller.js
+++ controllers/mail-controller.js
@@ -1,15 +1,34 @@
 const { sendMail } = require('../mail/send-mail');
+const axios = require('axios');
 
 class MailController {
   // отправка текста
   async sendText(req, res) {
-    const { name, email, text } = req.body;
+    const { name, email, text, token } = req.body;
 
-    if (![name, email, text].every(Boolean)) {
+    if (![name, email, text, token].every(Boolean)) {
       return res.status(403).json({});
     }
 
-    sendMail('dmidrok@gmail.com', { name, email, text });
+    const response = await axios.post(
+      'https://www.google.com/recaptcha/api/siteverify',
+      null,
+      {
+        params: {
+          response: token,
+          secret: process.env.RECAPTCHA_SECRET_KEY,
+        },
+      }
+    );
+
+    const { success } = response.data;
+
+    if (!success) {
+      console.log({ token, success }, 'FAILED');
+      return res.status(403).json({});
+    }
+
+    sendMail(process.env.EMAIL_ADMIN, { name, email, text });
 
     return res.json({
       result: true,
